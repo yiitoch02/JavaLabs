59a6924d7089e331261906d096a34874e5a3d845
yiitoch02 <2023142brak@aupp.edu.kh>
2023-07-07 15:52:24 +0700

    j

--- return_2_subpro.py
+++ return_2_subpro.py
@@ -1,13 +1,9 @@
-# function return more than 1 value
-# def return_2(a, b):
-#     x = a+b
-#     return x, b
 
+def return_2(a, b):
+     x = a+b
+     return x, b
 
-# new_x, new_b = return_2(12, 3)
-
-# print(new_x, new_b)
-
-# import random
-# lucky_number = random.randint(0, 10)
-# print(lucky_number)
+ new_x, new_b = return_2(12, 3)
+ print(new_x, new_b)
+ lucky_number = random.randint(0, 10)
+ print(lucky_number)
--- while_loop.py
+++ while_loop.py
@@ -1,27 +1,23 @@
-# x = 0
-# while x <= 10:  # declare condition
-#     x += 1  # increase x
-#     print(f"hello world! {x}")
+x = 0
+while x <= 10:  # declare condition
+    x += 1  # increase x
+    print(f"hello world! {x}")
 
-# live virtualization
-# https://pythontutor.com/visualize.html#code=x%20%3D%200%0Awhile%20x%20%3C%3D%2010%3A%20%20%23%20declare%20condition%0A%20%20%20%20x%20%2B%3D%201%20%20%23%20increase%20x%0A%20%20%20%20print%28f%22hello%20world!%20%7Bx%7D%22%29&cumulative=false&curInstr=35&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false
 
 # type your name:
 # break statement
-# while True:
-#     enter_name = input("type your name: ")
-#     if enter_name != "your name":
-#         print("wrong!")
-#     else:
-#         print("correct!")
+while True:
+    enter_name = input("type your name: ")
+    if enter_name != "your name":
+        print("wrong!")
+    else:
+       print("correct!")
 
-# continue statement
-# x = 0
-# while x <= 9:
-#     x += 1
-#     if x % 2 != 0:
-#         continue
-#     else:
-#         print(x)
 
-#     print("hitt even!!!")
+ x = 0
+ while x <= 9:
+     x += 1
+     if x % 2 != 0:
+        print(x)
+    else:
+         print("hitt even!!!")
